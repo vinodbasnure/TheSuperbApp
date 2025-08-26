d376dee498a8f474ab73a281439fd9404ddf7eca
vinodbasnure <basnurevinod@gmail.com>
2025-08-26 21:19:19 +0530

    Initial Commit

---
+++ TheSuperbApp/TheSuperbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSuperbAppApp.swift
+//  TheSuperbApp
+//
+//  Created by Vinod Basnure on 26/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TheSuperbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
